58e928f4029080701ccaaa770b2d4ab59b61aee9
inainthuothuvom <inainthuothuvom@gmail.com>
2026-06-25 04:33:21 +0530

    Remove unused dummyWorker

--- src/engine/dummyWorker.ts
+++
@@ -1,7 +0,0 @@
-/// <reference lib="webworker" />
-declare const self: DedicatedWorkerGlobalScope;
-
-self.onmessage = (e: MessageEvent) => {
-  const cmd = typeof e.data === 'string' ? e.data : '';
-  if (cmd) postMessage(cmd);
-};
